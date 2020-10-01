22fbd43ef2d7d92e3f22aa29e2764166fbf7e4e4
nikita <nikita.kochetkov@jetbrains.com>
2020-10-01 17:43:38 +0300

    solve base E cf

---
+++ CF/E.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    n = int(input())
+    for _ in range(n):
+        input()
+    input()
+    print(0.0)
+    print(0.0)
+    print(1.0)
+    print(1.0)
+    print(0.0)
+    print(0.0)
+    print(-5.0)
