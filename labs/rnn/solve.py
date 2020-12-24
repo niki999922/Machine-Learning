6d9240e6d9709d1f5c82036693a692f7f28acf65
PolinB <PolinB2413@gmail.com>
2020-12-24 13:14:43 +0300

    upload files for rnn

--- labs/rnn/solve.py
+++ labs/rnn/solve.py
@@ -1,14 +1,5 @@
-import numpy
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import Dropout
-from keras.layers import LSTM
-from keras.optimizers import SGD
 from keras.callbacks import ModelCheckpoint
-from keras.utils import np_utils
-import re
 import os
-import sys
 import numpy
 from keras.models import Sequential
 from keras.layers import Dense
@@ -16,7 +7,6 @@ from keras.layers import Dropout
 from keras.layers import LSTM
 from keras.optimizers import SGD
 from keras.utils import np_utils
-import re
 import re
 import numpy as np
 import sys
@@ -26,7 +16,7 @@ import sys
 SEQUENCE_LENGTH = 40
 EPOCHS = 30
 BATCH_SIZE = 64
-EPS = 0e-6
+EPS = 1e-6
 
 
 # Example input = "testText.txt"
@@ -53,42 +43,7 @@ def clear_file(input):
 
     return outputFile
 
-def teach_model(file, teached_model_folder):
-    print("Start teaching model", flush=True)
-    text = open(file).read()
-
-    chars_int_map = dict((c, i) for i, c in enumerate(sorted(list(set(text)))))
-    amount_chars, amount_different_chars = len(text), len(chars_int_map)
-
-    x_arr_dataset_custom_tmp, y_arr_dataset_custom_tmp = [], []
-
-    for i in range(amount_chars - SEQUENCE_LENGTH):
-        sequence_from, char_out = text[i:i + SEQUENCE_LENGTH], text[i + SEQUENCE_LENGTH]
-        x_arr_tmp = list(map(lambda char: chars_int_map[char], sequence_from))
-        x_arr_dataset_custom_tmp.append(x_arr_tmp)
-        y_arr_dataset_custom_tmp.append(chars_int_map[char_out])
-
-    x_arr_dataset = numpy.reshape(x_arr_dataset_custom_tmp, (len(x_arr_dataset_custom_tmp), SEQUENCE_LENGTH, 1))
-    x_arr_dataset = x_arr_dataset / float(amount_different_chars)
-    y_arr_dataset = np_utils.to_categorical(y_arr_dataset_custom_tmp)
-
-    sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
-
-    model = Sequential()
-    model.add(LSTM(256, input_shape=(x_arr_dataset.shape[1], x_arr_dataset.shape[2])))
-    model.add(Dropout(0.2))
-    model.add(Dense(y_arr_dataset.shape[1], activation='softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer=sgd)
-
-    filepath = f'results/{teached_model_folder}/' + "epoch_{epoch:02d}__loss_{loss:.4f}.hdf5"
-    checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
-    callbacks_list = [checkpoint]
-
-    model.fit(x_arr_dataset, y_arr_dataset, epochs=EPOCHS, batch_size=BATCH_SIZE, callbacks=callbacks_list)
-    print("End teaching model", flush=True)
-
-def generate_text_RNN(file, teached_model_file_path, amount_sequence=100):
-    print(f'Start generating text under teached model on {teached_model_file_path}', flush=True)
+def init_data(file):
     raw_text = open(file).read()
 
     chars = sorted(list(set(raw_text)))
@@ -107,14 +62,40 @@ def generate_text_RNN(file, teached_model_file_path, amount_sequence=100):
     x_arr_dataset = x_arr_dataset / float(amount_different_chars)
     y_arr_dataset = np_utils.to_categorical(y_arr_dataset_custom_tmp)
 
+    return x_arr_dataset, y_arr_dataset, x_arr_dataset_custom_tmp, int_chars_map
+
+def init_model(x_arr_dataset, y_arr_dataset):
     sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
 
     model = Sequential()
     model.add(LSTM(256, input_shape=(x_arr_dataset.shape[1], x_arr_dataset.shape[2])))
     model.add(Dropout(0.2))
     model.add(Dense(y_arr_dataset.shape[1], activation='softmax'))
-    model.load_weights(teached_model_file_path)
     model.compile(loss='categorical_crossentropy', optimizer=sgd)
+    return model
+
+def teach_model(file, teached_model_folder):
+    print("Start teaching model", flush=True)
+
+    x_arr_dataset, y_arr_dataset, _, _ = init_data(file)
+
+    model = init_model(x_arr_dataset, y_arr_dataset)
+
+    filepath = f'results/{teached_model_folder}/' + "epoch_{epoch:02d}__loss_{loss:.4f}.hdf5"
+    checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+    callbacks_list = [checkpoint]
+
+    model.fit(x_arr_dataset, y_arr_dataset, epochs=EPOCHS, batch_size=BATCH_SIZE, callbacks=callbacks_list)
+    print("End teaching model", flush=True)
+
+def generate_text_RNN(file, teached_model_file_path, amount_sequence=100):
+    print(f'Start generating text under teached model on {teached_model_file_path}', flush=True)
+
+    x_arr_dataset, y_arr_dataset, x_arr_dataset_custom_tmp, int_chars_map = init_data(file)
+    amount_different_chars = len(int_chars_map)
+
+    model = init_model(x_arr_dataset, y_arr_dataset)
+    model.load_weights(teached_model_file_path)
 
     start_sequence_id = numpy.random.randint(0, len(x_arr_dataset_custom_tmp) - 1)
     start_sequence = x_arr_dataset_custom_tmp[start_sequence_id]
@@ -132,54 +113,56 @@ def generate_text_RNN(file, teached_model_file_path, amount_sequence=100):
         start_sequence = start_sequence[1:len(start_sequence)]
     print(f'\nEnd generating text', flush=True)
 
-
-def get_windows(file, n, k, m):
-    text = open(file).read()
+def do_mark(file, n, k, m):
     print(f'Start generating text for {file}', flush=True)
-    print(f'n = {n}', flush=True)
-    print(f'k = {k}', flush=True)
-    print(f'm = {m}', flush=True)
-    w = set()
-    for i in range(len(text) - n + 1):
-        w.add(text[i:i + n])
-    print("Number of windows: ", len(w))
+    text = open(file).read()
+    windows = get_windows(text, n)
+    graph, w_to_int, int_to_w = get_graph(windows, text, n)
+    start_line = get_random_start(text)
+    generating_by_graph(n, k, m, start_line, graph, w_to_int, int_to_w)
 
-    w_to_int = dict((c, i) for i, c in enumerate(w))
-    int_to_w = dict((i, c) for i, c in enumerate(w))
-    matrix = [[0 for _ in range(len(w))] for _ in range(len(w))]
+def get_windows(text, n):
+    windows = set()
+    for i in range(len(text) - n + 1):
+        windows.add(text[i:i + n])
+    print(f'Windows: {len(windows)}', flush=True)
+    return windows
+
+def get_graph(windows, text, n):
+    window_to_int = dict((c, i) for i, c in enumerate(windows))
+    int_to_wwindow = dict((i, c) for i, c in enumerate(windows))
+    matrix = [[0 for _ in range(len(windows))] for _ in range(len(windows))]
     for i in range(len(text) - n):
         cur_w = text[i:i + n]
         next_w = text[(i + 1):(i + n + 1)]
-        matrix[w_to_int[cur_w]][w_to_int[next_w]] += 1
+        matrix[window_to_int[cur_w]][window_to_int[next_w]] += 1
     matrix = np.array(norm_matrix(matrix))
-    # print(np.array(matrix))
+    return matrix, window_to_int, int_to_wwindow
 
+def get_random_start(text):
     lines = text.split('\n')
     start = np.random.randint(0, len(lines) - 1)
-    if len(lines[start]) < k:
-        print(f'So big {k} for "{lines[start]}"')
-    elif k < n:
-        print(f'Must k > m"')
-    else:
-        # prefix = lines[start][0:k]
-        prefix = "А за окном"
-        print(f'Start:\n{prefix}')
-        print("Gen:")
-        start_window = prefix[len(prefix) - n:]
-        for i in range(m):
-            sug_next_pos = get_all_by_max(matrix[w_to_int[start_window]])
-            if len(sug_next_pos) == 0:
-                print(f'End after {i} symbols')
-                break
-            elif len(sug_next_pos) == 1:
-                ind = 0
-            else:
-                ind = np.random.randint(0, len(sug_next_pos) - 1)
-            next_pos = sug_next_pos[ind]
-            # print(next_pos)
-            start_window = int_to_w[next_pos]
-            sys.stdout.write(start_window[len(start_window) - 1])
-    print('End generating text', flush=True)
+    return lines[start]
+
+# K have to more M
+def generating_by_graph(n, k, m, start_line, matrix, w_to_int, int_to_w):
+    prefix = start_line[0:k]
+    print(f'Start phrase:\n{prefix}', flush=True)
+    print("Generating:", flush=True)
+    start_window = prefix[len(prefix) - n:]
+    for i in range(m):
+        sug_next_pos = get_all_by_max(matrix[w_to_int[start_window]])
+        if len(sug_next_pos) == 0:
+            print(f'Can\'t continue', flush=True)
+            break
+        elif len(sug_next_pos) == 1:
+            ind = 0
+        else:
+            ind = np.random.randint(0, len(sug_next_pos) - 1)
+        next_pos = sug_next_pos[ind]
+        start_window = int_to_w[next_pos]
+        sys.stdout.write(start_window[len(start_window) - 1])
+    print('\nEnd generating text', flush=True)
 
 
 def get_all_by_max(data):
@@ -207,16 +190,22 @@ if __name__ == '__main__':
     # raw_file = "testText"
     # teached_model_folder = "testFolder"
     # teached_model = "epoch_30__loss_3.1055.hdf5"
-    raw_file = "evgeny_onegin"
-    teached_model_folder = "evgeny"
-    teached_model = "epoch_30__loss_2.4439.hdf5"
+
+    # raw_file = "evgeny_onegin"
+    # teached_model_folder = "evgeny"
+    # teached_model = "epoch_30__loss_2.4439.hdf5"
+
+    raw_file = "prestuplenie_i_nakazanie"
+    teached_model_folder = "radion"
+    teached_model = "epoch_30__loss_1.8504.hdf5"
+
     teached_model_file_path = f'results/{teached_model_folder}/{teached_model}'
 
     cleared_file = clear_file(raw_file)
     # teach_model(cleared_file, teached_model_folder)
 
-    # generate_text_RNN(cleared_file, teached_model_file_path, amount_sequence=120)
+    generate_text_RNN(cleared_file, teached_model_file_path, amount_sequence=240)
 
-    get_windows(cleared_file, 5, 10, 200)
+    # do_mark(cleared_file, 4, 10, 200)
 
 
